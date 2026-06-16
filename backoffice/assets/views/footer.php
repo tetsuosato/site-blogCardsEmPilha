5a7144d577ab58e77b5b60b8d713633570a7f0e8
tetsuosato <pablosato666@gmail.com>
2026-06-15 22:30:35 -0300

    Backoffice login

---
+++ backoffice/assets/views/footer.php
@@ -0,0 +1,7 @@
+<footer class="container-fluid">
+    
+</footer>
+
+<!-- Bootstrap Bundle with Popper -->
+<script src="<?= BASE_URL ?>/assets/bootstrap/5.3.8/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+
